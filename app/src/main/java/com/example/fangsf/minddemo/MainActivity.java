7fd3ab2828799acdf5a17008ad724c1b1fdb9fdf
fangsf <highestFeng@gmail.com>
2017-11-19 16:05:00 +0800

    init

--- app/src/main/java/com/example/fangsf/minddemo/MainActivity.java
+++ app/src/main/java/com/example/fangsf/minddemo/MainActivity.java
@@ -9,9 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      
+        
+        
         
         
       
+        
+        
         
         
     }
