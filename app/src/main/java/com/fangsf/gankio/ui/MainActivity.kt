97e190f49dc8e5a1a071c55f276922d8be3849e1
fangsf <highestfeng@gmail.com>
2019-10-08 23:59:41 +0800

    删除所有代码, 增加navigation库

---
+++ app/src/main/java/com/fangsf/gankio/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.fangsf.gankio.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by fangsf on 2019-10-08
+ * Useful:
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
